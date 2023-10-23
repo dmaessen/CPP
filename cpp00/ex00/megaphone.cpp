40af0b4fc0e65827d9e25eb9375b1915d42729b5
domi <dmaessen@student.codam.nl>
2023-10-23 16:01:32 +0200

    added new files

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << '\n';
+	else
+	{
+		for (int i = 1;  i < argc; ++i)
+		{
+			for (size_t len = 0; len < strlen(argv[i]); len++)
+				putchar(toupper(argv[i][len]));
+		}
+		std::cout << '\n';
+	}
+	return 0;
+}
+
+// do i need to call on a constructor and destructor ??
