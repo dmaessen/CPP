c2b01e6fd59595455ea7dd7194e0da3a1a1570a9
domi <dmaessen@student.codam.nl>
2024-02-26 15:17:50 +0100

    work in prog

--- cpp09/ex00/src/main.cpp
+++ cpp09/ex00/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmaessen <dmaessen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: domi <domi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 10:28:11 by dmaessen          #+#    #+#             */
-/*   Updated: 2024/02/23 13:22:49 by dmaessen         ###   ########.fr       */
+/*   Updated: 2024/02/26 15:17:31 by domi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <ctime>
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        std::cout << "Wrong amount of arguments.\n";
+        std::cout << "Error: wrong amount of arguments\n";
         return 1;
     }
     
@@ -40,15 +41,39 @@ int main(int argc, char **argv) {
         date = line.substr(0, 10);
 		value = line.substr(11, line.size());
 		nb = std::stof(value);
+		m[date] = nb;
+    }
+    infile.close();
+	// end loading database
+
+	// for input file
+	infile.open(argv[1], std::ios::in);
+    if (!infile.is_open()) {
+        std::cout << "Error: opening input file\n";
+        return 1;
+    }
+	char* date1;
+	std::string value;
+	float nb;
+    char* line;
+	std::time_t t = std::time(nullptr);
+	std::getline(infile, line);
+    while (std::getline(infile, line)){
+        date1 = line.substr(0, 10);
+		if (!strftime(date1, sizeof(date1), "%d/%m/%Y", std::localtime(&t)) // or strptime??
+			std::cout << "Error: date is incorrect\n";
+		value = line.substr(11, line.size());
+		nb = std::stof(value);
 		if (nb < 0 || nb > 1000){
-			std::cout << "Incorrect value, needs to be between 0 and 1000\n";
+			std::cout << "Error: incorrect value, needs to be between 0 and 1000\n";
 			break ;
 		}
 		else {
 			m[date] = nb;
 		}
     }
-    infile.close();
+	
+	
 
 	//TESTING
 	// std::map<std::string, float>::iterator it = m.begin();
